e32c19fcf05f9871b17c2df8c3781a3b123dcdea
Niklas <n2.muller@student.vu.nl>
2021-10-08 13:33:23 +0200

    Fixed trials loading

--- parameter_optimization.py
+++ parameter_optimization.py
@@ -90,23 +90,25 @@ def optimize(method, generations, cpus, enemies, trials, max_trials):
     if not path.exists():
         path.mkdir(parents=True, exist_ok=True)
 
+    max_trials = max_trials + len(trials.trials)
+
     return fmin(partial(objective, method=method, generations=generations, cpus=cpus, group=enemies, run_path=path)
                 , search_space(), algo=tpe.suggest, trials=trials, max_evals=max_trials), trials
-
+    
 
 if __name__ == '__main__':
     if platform.system() == 'Darwin':
         multiprocessing.set_start_method('spawn')  # Comment this if not on MACOS
 
 
-    method = "FS_NEAT"
-    generations = 1
-    cpus = 2
-    enemies = [1, 2, 4, 5]
+    method = "ENGINEERED"
+    generations = 100
+    cpus = multiprocessing.cpu_count() - 2
+    enemies = [2, 4, 5, 6]
     enemy_string = "".join([str(x) for x in enemies])
     # trial_steps = 1
 
-    trials = None
+    trials = Trials()
 
     try:
         max_trials = int(sys.argv[1])
@@ -129,15 +131,17 @@ if __name__ == '__main__':
 
     print(f"Running {max_trials} trials")
 
+
     solution, trials = optimize(method, generations, cpus, enemies, trials, max_trials)
+
 
     result_path = Path(f"param_opt_results/{method}/{enemy_string}")
     if not result_path.exists():
         result_path.mkdir(parents=True, exist_ok=True)
 
-    with open(f"{result_path}/trials.p", "wb") as f:
-        pickle.dump(trials, f)
+    # with open(f"{result_path}/trials.p", "wb") as f:
+    pickle.dump(trials, open(f"{result_path}/trials.p", "wb"))
 
-    with open(f"{result_path}/best_solution.p", "wb") as f:
-        pickle.dump(solution, f)
+    # with open(f"{result_path}/best_solution.p", "wb") as f:
+    pickle.dump(solution, open(f"{result_path}/best_solution.p", "wb"))
 
